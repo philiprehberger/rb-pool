b7ae8b309fc83299ba51c06fb14ddb786e1bf28c
Reverend Philip <me@philiprehberger.com>
2017-01-08 16:54:29 -0600

    Release v0.1.3

--- spec/philiprehberger/pool_spec.rb
+++ spec/philiprehberger/pool_spec.rb
@@ -191,6 +191,159 @@ RSpec.describe Philiprehberger::Pool do
       it 'requires a positive size' do
         expect { described_class.new(size: 0) { 'x' } }.to raise_error(ArgumentError)
       end
+
+      it 'raises for negative size' do
+        expect { described_class.new(size: -1) { 'x' } }.to raise_error(ArgumentError)
+      end
+    end
+
+    describe 'multiple sequential checkouts' do
+      it 'allows checking out up to max size resources sequentially' do
+        pool = described_class.new(size: 3, &factory)
+        r1 = pool.checkout
+        r2 = pool.checkout
+        r3 = pool.checkout
+
+        expect(r1).not_to be_nil
+        expect(r2).not_to be_nil
+        expect(r3).not_to be_nil
+        expect(pool.stats[:in_use]).to eq(3)
+        expect(pool.stats[:available]).to eq(0)
+      end
+    end
+
+    describe 'checkout returns different objects' do
+      it 'returns distinct resource objects' do
+        pool = described_class.new(size: 3, &factory)
+        r1 = pool.checkout
+        r2 = pool.checkout
+
+        expect(r1).not_to equal(r2)
+      end
+    end
+
+    describe 'checkin makes object available again' do
+      it 'reuses a checked-in resource' do
+        pool = described_class.new(size: 1, &factory)
+        r1 = pool.checkout
+        pool.checkin(r1)
+
+        r2 = pool.checkout
+        expect(r2).to equal(r1)
+      end
+    end
+
+    describe 'factory block call count' do
+      it 'calls factory exactly once per unique resource created' do
+        call_count = [0]
+        pool = described_class.new(size: 3) do
+          call_count[0] += 1
+          "resource-#{call_count[0]}"
+        end
+
+        pool.checkout
+        pool.checkout
+        expect(call_count[0]).to eq(2)
+      end
+
+      it 'does not call factory when reusing checked-in resources' do
+        call_count = [0]
+        pool = described_class.new(size: 2) do
+          call_count[0] += 1
+          "resource-#{call_count[0]}"
+        end
+
+        r = pool.checkout
+        pool.checkin(r)
+        pool.checkout
+        expect(call_count[0]).to eq(1)
+      end
+    end
+
+    describe 'with block exception safety' do
+      it 'returns resource after StandardError' do
+        pool = described_class.new(size: 1, &factory)
+
+        expect { pool.with { raise StandardError, 'oops' } }.to raise_error(StandardError, 'oops')
+        expect(pool.stats[:available]).to eq(1)
+        expect(pool.stats[:in_use]).to eq(0)
+      end
+
+      it 'returns resource after custom error class' do
+        custom_error = Class.new(StandardError)
+        pool = described_class.new(size: 1, &factory)
+
+        expect { pool.with { raise custom_error, 'custom' } }.to raise_error(custom_error)
+        expect(pool.stats[:available]).to eq(1)
+      end
+
+      it 'allows reuse after exception in with block' do
+        pool = described_class.new(size: 1, &factory)
+
+        expect { pool.with { raise 'fail' } }.to raise_error(RuntimeError)
+
+        result = pool.with { |r| r }
+        expect(result).not_to be_nil
+        expect(pool.stats[:available]).to eq(1)
+      end
+    end
+
+    describe 'double checkin handling' do
+      it 'ignores a second checkin of the same resource' do
+        pool = described_class.new(size: 2, &factory)
+        r = pool.checkout
+        pool.checkin(r)
+        pool.checkin(r)
+
+        expect(pool.stats[:available]).to eq(1)
+      end
+    end
+
+    describe 'pool behavior after shutdown' do
+      it 'sets stats to zero after shutdown' do
+        pool = described_class.new(size: 2, &factory)
+        pool.checkout
+        pool.shutdown
+
+        expect(pool.stats[:size]).to eq(0)
+        expect(pool.stats[:available]).to eq(0)
+        expect(pool.stats[:in_use]).to eq(0)
+      end
+
+      it 'calls close on resources during shutdown' do
+        closed = []
+        pool = described_class.new(size: 2) do
+          obj = double('closeable')
+          allow(obj).to receive(:close) { closed << obj }
+          obj
+        end
+
+        pool.checkout
+        pool.shutdown
+
+        expect(closed.size).to eq(1)
+      end
+    end
+
+    describe 'stats accuracy' do
+      it 'reflects correct state after mixed operations' do
+        pool = described_class.new(size: 3, &factory)
+        r1 = pool.checkout
+        r2 = pool.checkout
+
+        expect(pool.stats).to eq({ size: 2, available: 0, in_use: 2 })
+
+        pool.checkin(r1)
+        expect(pool.stats).to eq({ size: 2, available: 1, in_use: 1 })
+
+        pool.checkin(r2)
+        expect(pool.stats).to eq({ size: 2, available: 2, in_use: 0 })
+      end
+
+      it 'reports size 0 for a fresh pool' do
+        pool = described_class.new(size: 5, &factory)
+        expect(pool.stats).to eq({ size: 0, available: 0, in_use: 0 })
+      end
     end
   end
 end
